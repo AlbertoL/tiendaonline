dd2b6e74c7644a453691de2c976618727ac4c1b8
Alberto <espiritudeelias@gmail.com>
2014-08-09 02:43:18 -0400

    Tienda On-Line / modulos

---
+++ php/cabecera.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Tienda OnLine</title>
+	<link rel="stylesheet" href="../css/normalize.css">
+	<link rel="stylesheet" href="css/movil.css" media='screen and (min-width: 1px) and (max-width: 800px)'/>
+	<link rel="stylesheet" href="css/pcmac.css" media='screen and (min-width: 801px) and (max-width: 4000px)'/>
+</head>
+<body>
+	<div id="contenedor">
+		<header>
+			<a href="index.php">
+				<h1>Tienda</h1>
+			</a>
+			<h2>Subtítulo de la tienda</h2>
+		</header>
+		<section>
---
+++ php/piedepagina.php
@@ -0,0 +1,7 @@
+</section>
+		<footer>
+			<h6>Alberto López 2014</h6>
+		</footer>
+	</div>
+</body>
+</html>
